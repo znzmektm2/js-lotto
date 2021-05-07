ca00036f6e91ec9d6c2e01ae6f2cf0bdeeb4aea8
znzmektm2 <tingcber2@naver.com>
2021-05-07 23:27:42 +0900

    feat: 수익률 기능 구현

--- src/js/controllers/LottoController.js
+++ src/js/controllers/LottoController.js
@@ -3,7 +3,7 @@ import PriceFormView from "./../views/PriceFormView.js";
 import PurchasedLottosView from "./../views/PurchasedLottosView.js";
 import WinningNumbersView from "./../views/WinningNumbersView.js";
 import ModalView from "./../views/ModalView.js";
-import { PRICE, MSG } from "./../utils/constants.js";
+import { PRICE, MSG , PRIZZE_MONEY} from "./../utils/constants.js";
 import LottoModel from "./../models/LottoModel.js";
 import { winningNumbers } from "../utils/utils.js";
 
@@ -18,7 +18,7 @@ export default class LottoController {
     this.$modalView = new ModalView($("#modalWrap"));
     this.lottoModel = new LottoModel();
     this.winningInputs = $All("#winningNumbersWrap input");
-    this.result = {};
+    this.winningResult = {};
     this.isChecked = false;
     this.bindEvents();
   }
@@ -46,8 +46,9 @@ export default class LottoController {
       this.$priceFormView.reset();
       this.$purchasedLottosView.reset();
       this.$winningNumbersView.reset(this.winningInputs);
-      this.$modalView.reset(this.result);
-      this.result = {}
+      this.$modalView.reset(this.winningResult);
+      this.price = 0;
+      this.winningResult = {}
       this.isChecked = false;
     });
   }
@@ -59,7 +60,8 @@ export default class LottoController {
   }
 
   submitPrice(e) {
-    const price = +e.target.elements.priceInput.value;
+    this.price = +e.target.elements.priceInput.value;
+    const price = this.price;
 
     if (price > PRICE.MAX || price < PRICE.UNIT)
       return alert(MSG.EXCEED_PRICE_RANGE);
@@ -79,27 +81,33 @@ export default class LottoController {
 
     if (this.winningNumbers.length !== this.winningInputs.length)
       return alert(MSG.INVALID_NUMBERS);
+    this.$modalView.show();
+
+    if (!!this.isChecked) return;
 
     this.winningInputs.forEach((input) => {
       input.disabled = true;
     });
-    this.winningResult();
-    this.$modalView.show();
+    this.insertWinningResult();
+    this.insertProfitResult();
+
+    this.isChecked = true;
   }
 
-  winningResult() {
+
+  insertWinningResult() {
     const winningLotto = this.winningNumbers;
+
     const lottos = this.lottoModel.lottos;
-    let result = this.result;
 
-    if (!!this.isChecked) return;
-
+    let winningResult = this.winningResult;
     lottos.forEach((lotto, i) => {
+
       const count = winningLotto.reduce((count, winningNumber, i) => {
         if (count === 5 && i === 6) {
-          result.BONUS_BALL
-              ? (result.BONUS_BALL = result.BONUS_BALL + 1)
-              : (result.BONUS_BALL = 1);
+          winningResult.BONUS_BALL
+              ? (winningResult.BONUS_BALL = winningResult.BONUS_BALL + 1)
+              : (winningResult.BONUS_BALL = 1);
 
           return "BONUS_BALL";
         }
@@ -107,17 +115,31 @@ export default class LottoController {
 
         return count;
       }, 0);
-
       if (count < 3 || count === "BONUS_BALL") return;
 
-      result[count] ? (result[count] = result[count] + 1) : (result[count] = 1);
-    });
+      winningResult[count] ? (winningResult[count] = winningResult[count] + 1) : (winningResult[count] = 1);
 
-    for (const count in result) {
-      $(`#win_${count}`).innerText = result[count]+'개';
+    });
+    for (const count in winningResult) {
+      $(`#win_${count}`).innerText = winningResult[count]+'개';
+
+    }
+    console.log("winningResult", winningResult);
+  }
+
+  insertProfitResult() {
+    let totalPrizeMoney = 0;
+    const winningResult = this.winningResult;
+
+    for (const count in winningResult) {
+      totalPrizeMoney += PRIZZE_MONEY[count] * winningResult[count];
+      console.log('RIZZE_MONEY[count]:',PRIZZE_MONEY[count], 'winningResult[count]:', winningResult[count])
+      console.log('totalPrizeMoney',totalPrizeMoney)
     }
 
-    this.isChecked = true;
-    console.log("result", result);
+    console.log('profit:', totalPrizeMoney / this.price * 100)
+
+    $('#profit').innerText = totalPrizeMoney / this.price * 100;
   }
+
 }
