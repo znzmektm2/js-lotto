d7e77b44d5a6be2522ca54a87fb9a406504501a9
znzmektm2 <tingcber2@naver.com>
2021-05-15 23:54:55 +0900

    refactor: utills 함수 리팩토링

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,4 +1,4 @@
-import { LOTTO } from "./constants.js";
+import { LOTTO, PRIZZE_MONEY } from "./constants.js";
 
 const randomNumber = () => {
   return Math.floor(Math.random() * (LOTTO.MAX + 1 - LOTTO.MIN)) + LOTTO.MIN;
@@ -21,4 +21,38 @@ export const winningNumbers = (winningInputs) => {
   });
 
   return [...winningNums];
+};
+
+export const insertWinningResult = (winningLotto, lottos, winningResult) => {
+  lottos.forEach((lotto, i) => {
+    const count = winningLotto.reduce((count, winningNumber, i) => {
+      if (count === 5 && i === 6) {
+        winningResult.BONUS_BALL
+          ? (winningResult.BONUS_BALL = winningResult.BONUS_BALL + 1)
+          : (winningResult.BONUS_BALL = 1);
+
+        return "BONUS_BALL";
+      }
+      if (lotto.includes(winningNumber)) count++;
+
+      return count;
+    }, 0);
+    if (count < 3 || count === "BONUS_BALL") return;
+
+    winningResult[count]
+      ? (winningResult[count] = winningResult[count] + 1)
+      : (winningResult[count] = 1);
+  });
+
+  return winningResult;
+};
+
+export const insertProfitResult = (winningResult, price) => {
+  let totalPrizeMoney = 0;
+
+  for (const count in winningResult) {
+    totalPrizeMoney += PRIZZE_MONEY[count] * winningResult[count];
+  }
+
+  return (totalPrizeMoney / price) * 100;
 };
