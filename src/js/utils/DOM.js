2514771828971a2af75052ce7db2cb24f6924569
znzmektm2 <tingcber2@naver.com>
2021-05-18 23:37:56 +0900

    rafactor: DOM.js 수정

--- src/js/utils/DOM.js
+++ src/js/utils/DOM.js
@@ -1,2 +1,3 @@
 export const $ = (selector) => document.querySelector(selector);
 export const $All = (selector) => document.querySelectorAll(selector);
+export const $winningInputs = $All("#winningNumbersWrap input");
